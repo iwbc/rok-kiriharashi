1a17f015f0e91da9fe54a892626774105670aadd
iwbc <1209760+iwbc@users.noreply.github.com>
2023-02-24 01:53:18 +0900

    templatesパス変更

--- bot.py
+++ bot.py
@@ -1,5 +1,6 @@
 # This Python file uses the following encoding: utf-8
 
+import os
 import sys
 from android_auto_play_opencv import AapoManager
 
@@ -32,7 +33,7 @@ def main():
         print("無効な入力値です。処理を中止します。")
         sys.exit(1)
 
-    template_dir_path = "templates/"
+    template_dir_path = os.path.join(os.path.dirname(__file__), "templates")
 
     print(f"\n===== 探索開始（CTRL+Cで終了） =====\n")
 
@@ -42,12 +43,12 @@ def main():
 def auto_fog():
     while True:
         print(f"\n===== 派遣可能になるまで待機 =====\n")
-        checkImg(template_dir_path + "scout_explore.png", infinite=True)
+        checkImg(os.path.join(template_dir_path, "scout_explore.png"), infinite=True)
         aapo.sleep(3)
 
         print(f"\n===== 霧選択 =====\n")
         try:
-            checkImg(template_dir_path + "scout_explore.png")
+            checkImg(os.path.join(template_dir_path, "scout_explore.png"))
         except TimeoutError:
             goToScoutCamp()
             aapo.sleep(1)
@@ -56,7 +57,7 @@ def auto_fog():
 
         print(f"\n===== 斥候派遣 =====\n")
         try:
-            checkImg(template_dir_path + "scout_send.png")
+            checkImg(os.path.join(template_dir_path, "scout_send.png"))
         except TimeoutError:
             goToScoutCamp()
             aapo.sleep(1)
@@ -68,12 +69,12 @@ def auto_fog():
 
 def goToScoutCamp():
     print(f"\n===== 都市に戻る =====\n")
-    checkImg(template_dir_path + "home.png")
+    checkImg(os.path.join(template_dir_path, "home.png"))
     aapo.sleep(5)
     print(f"\n===== 斥候キャンプ =====\n")
     aapo.touchPos(SCOUT_CAMP_TAP_POS[0], SCOUT_CAMP_TAP_POS[1])
     aapo.sleep(1)
-    checkImg(template_dir_path + "scout.png")
+    checkImg(os.path.join(template_dir_path, "scout.png"))
 
 
 def checkImg(
